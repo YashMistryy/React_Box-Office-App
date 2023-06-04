2787a17f7724c41aa01e874ee7be4e7a3e7577e7
yash mistry <mistryy37@gmail.com>
2023-06-04 17:29:54 +0530

    cleaned up the source folder from initial setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+// import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Helllo Worl
+    </div>
+  );
+}
+
+export default App;
